6ae9db45984bc979b1b5b5ec84157977d2a14bc0
qh2244 <qifenghuang@outlook.com>
2024-10-01 18:48:15 -0700

    Added failed tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,56 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_vest_item_should_decrease_after_one_day(self):
+
+        vest = "+5 Dexterity Vest"
+        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6), ]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.get_items_by_name(vest) == [Item(vest, 0, 1), Item(vest, 8, 18), Item(vest, 3, 5)]
+
+    def test_aged_brie_should_increase_quality_after_one_day(self):
+        brie = "Aged Brie"
+        items = [Item(brie, 2, 0), Item(brie, 1, 1), Item(brie, 0, 2)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.get_items_by_name(brie) == [Item(brie, 1, -1), Item(brie, 0, 0), Item(brie, -1, 1)]
+
+
+    def test_backstage_passes_quality_increase(self):
+        pass_item = "Backstage passes to a TAFKAL80ETC concert"
+        items = [Item(pass_item, 15, 20), Item(pass_item, 10, 30), Item(pass_item, 5, 40)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.get_items_by_name(pass_item) == [Item(pass_item, 14, 20), Item(pass_item, 9, 30), Item(pass_item, 4, 40)]
+
+
+    def test_sulfuras_quality_should_not_change(self):
+        sulfuras = "Sulfuras, Hand of Ragnaros"
+        items = [Item(sulfuras, 0, 80), Item(sulfuras, -1, 80)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.get_items_by_name(sulfuras) == [Item(sulfuras, -1, 79), Item(sulfuras, -2, 79)]
+
+
+
+    def test_conjured_item_quality_decreases_faster(self):
+        conjured = "Conjured Mana Cake"
+        items = [Item(conjured, 3, 6), Item(conjured, 1, 5), Item(conjured, 0, 4)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.get_items_by_name(conjured) == [Item(conjured, 2, 5), Item(conjured, 0, 4), Item(conjured, -1, 3)]
+
 
 if __name__ == '__main__':
     unittest.main()
